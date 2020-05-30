77b444150ba7d877e7ca2652b2160f3c7f82a5a1
Christa <christatingzon@gmail.com>
2020-05-31 01:17:55 +0800

    Update home page layout to bootstrap

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Breed from './components/Breed';
+
 import './App.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-	const [breeds] = useState([
-		{
-			id: 1,
-			text: 'Abbysian',
-			description: 'Cute, soft'
-		},
-		{
-			id: 2,
-			text: 'Wild Cat',
-			description: 'Wild, big'
-		},
-		{
-			id: 3,
-			text: 'Syberia',
-			description: 'Fluffy, soft'
-		},
-	]);
+	useEffect(() => {
+		fetchItems();
+	}, []);
+
+	const [breeds, setBreeds] = useState([]);
+
+	const fetchItems = async () => {
+		const res = await fetch('https://api.thecatapi.com/v1/breeds');
+		const items = await res.json();
+
+		setBreeds(items);
+	}
 
   return (
-    <div className="App">
-      <h1>Cat Browser</h1>
-      <Breed breeds={breeds} />
-    </div>
+  	<div className="home">
+	    <Container>
+	      <h1>Cat Browser</h1>
+	      <Row>
+	      	<Col xs={12} sm={6} md={3}>
+	      		<Form.Group controlId="formBreedSelect">
+	      			<Form.Label>Breed</Form.Label>
+	      			<Form.Control as="select" onChange={}>
+	      			  <option value>Select breed</option>
+	      			  {breeds.map(item => (
+	      			  	<option key={item.id} value="{item.id}">{item.name}</option>
+	      			  	))}
+	      			</Form.Control>
+				    </Form.Group>
+			    </Col>
+	     	</Row>
+	    </Container>
+	  </div>
   );
 }
 
