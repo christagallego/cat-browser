cbd5597a24d3ae4efc50db7735389b8b5f6f6b71
Christa <christatingzon@gmail.com>
2020-06-01 01:22:20 +0800

    Remove unused definition

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
